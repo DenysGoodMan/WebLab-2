9b5f437c1bd9ec8b88c2dbe1ce8a1da58b808c39
DenysGoodMan <hromskyidenys@gmail.com>
2024-04-02 18:38:32 +0300

    First commit

---
+++ JavaScript/my-slider.js
@@ -0,0 +1,18 @@
+$(function(){
+
+    $('.model_slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1, 
+        //dots: true,
+        //speed: 300
+    });
+
+   /* let button = document.getElementById("SUV");
+
+    button.slick({
+        slidesToShow: 3,
+        slidesToScroll: 19
+    });*/
+    
+});
+
